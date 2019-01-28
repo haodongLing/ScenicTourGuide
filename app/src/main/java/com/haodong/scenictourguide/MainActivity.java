2312c430c8c01ec4772abe4eec9d0578d7120a83
linghailong <linghailong@amberweather.com>
2019-01-28 19:18:59 +0800

    bottom navigation

--- app/src/main/java/com/haodong/scenictourguide/MainActivity.java
+++ app/src/main/java/com/haodong/scenictourguide/MainActivity.java
@@ -1,16 +1,48 @@
 package com.haodong.scenictourguide;
 
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.haodong.scenictourguide.common.app.Activity;
 
 import butterknife.BindView;
 
-import static com.haodong.scenictourguide.R.drawable.selector_tab_main_icon;
+public class MainActivity extends Activity implements View.OnClickListener {
+    @BindView(R.id.tab_bottom_main)
+    LinearLayout tabMain;
+    @BindView(R.id.tab_bottom_location)
+    LinearLayout tabLocation;
+    @BindView(R.id.tab_bottom_hotel)
+    LinearLayout tabHotel;
+    @BindView(R.id.tab_bottom_track)
+    LinearLayout tabTrack;
+    @BindView(R.id.tab_bottom_mine)
+    LinearLayout tabMine;
 
-public class MainActivity extends Activity {
-    @BindView(R.id.navigation)
-    AHBottomNavigation bottomNavigation;
+    @BindView(R.id.iv_tab_main)
+    ImageView mIvMain;
+    @BindView(R.id.iv_tab_location)
+    ImageView mIvLocation;
+    @BindView(R.id.iv_tab_hotel)
+    ImageView mIvHotel;
+    @BindView(R.id.iv_tab_track)
+    ImageView mIvTrack;
+    @BindView(R.id.iv_tab_mine)
+    ImageView mIvMine;
+
+    @BindView(R.id.tv_main_main)
+    TextView mTvMain;
+    @BindView(R.id.tv_main_location)
+    TextView mTvLocation;
+    @BindView(R.id.tv_main_hotel)
+    TextView mTvHotel;
+    @BindView(R.id.tv_main_track)
+    TextView mTvTrack;
+    @BindView(R.id.tv_main_mine)
+    TextView mTvMine;
 
     @Override
     protected int getContentLayoutId() {
@@ -20,31 +52,52 @@ public class MainActivity extends Activity {
 
     @Override
     protected void initWidget() {
-        // Create items
-        AHBottomNavigationItem tabMain = new AHBottomNavigationItem(getResources().getString(R
-                .string.navigation_main), R.drawable.selector_tab_main_icon);
-        AHBottomNavigationItem tabLocation = new AHBottomNavigationItem(getResources().getString(R
-                .string.navigation_location), R.drawable.selector_tab_location_icon);
-        AHBottomNavigationItem tabHotel = new AHBottomNavigationItem(getResources().getString(R
-                .string.navigation_hotel), R.drawable.selector_tab_hotel_icon);
-        AHBottomNavigationItem tabTrack = new AHBottomNavigationItem(getResources().getString(R
-                .string.navigation_track), R.drawable.selector_tab_track_icon);
-        AHBottomNavigationItem tabMine = new AHBottomNavigationItem(getResources().getString(R
-                .string.navigation_mine), R.drawable.selector_tab_mine_icon);
+        tabMain.setOnClickListener(this);
+        tabLocation.setOnClickListener(this);
+        tabHotel.setOnClickListener(this);
+        tabTrack.setOnClickListener(this);
+        tabMine.setOnClickListener(this);
 
+    }
 
-// Add items
-        bottomNavigation.addItem(tabMain);
-        bottomNavigation.addItem(tabLocation);
-        bottomNavigation.addItem(tabHotel);
-        bottomNavigation.addItem(tabTrack);
-        bottomNavigation.addItem(tabMine);
-        bottomNavigation.setForceTint(false);
-        // 使用圆圈效果
-        bottomNavigation.setColored(false);
-        bottomNavigation.setSelectedBackgroundVisible(true);
-        bottomNavigation.setUseElevation(true);
-        // Set current item programmatically
-        bottomNavigation.setCurrentItem(0);
+    @Override
+    public void onClick(View v) {
+        resetTabState();
+        switch (v.getId()) {
+            case R.id.tab_bottom_main:
+                mIvMain.setImageResource(R.drawable.ic_main_color);
+                mTvMain.setTextColor(Color.parseColor("#EE7C3D"));
+                break;
+            case R.id.tab_bottom_location:
+                mIvLocation.setImageResource(R.drawable.ic_location);
+                mTvLocation.setTextColor(Color.parseColor("#EE7C3D"));
+                break;
+            case R.id.tab_bottom_hotel:
+                mIvHotel.setImageResource(R.drawable.ic_hotel_color);
+                mTvHotel.setTextColor(Color.parseColor("#EE7C3D"));
+                break;
+            case R.id.tab_bottom_track:
+                mIvTrack.setImageResource(R.drawable.ic_track_color);
+                mTvTrack.setTextColor(Color.parseColor("#EE7C3D"));
+                break;
+            case R.id.tab_bottom_mine:
+                mIvMine.setImageResource(R.drawable.ic_mine_color);
+                mTvMine.setTextColor(Color.parseColor("#EE7C3D"));
+                break;
+        }
+    }
+
+    private void resetTabState() {
+        mIvMain.setImageResource(R.drawable.ic_main_normal);
+        mTvMain.setTextColor(getResources().getColor(R.color.colorTxtNormal));
+        mIvLocation.setImageResource(R.drawable.ic_location_normal);
+        mTvLocation.setTextColor(getResources().getColor(R.color.colorTxtNormal));
+        mIvHotel.setImageResource(R.drawable.ic_hotel_normal);
+        mTvHotel.setTextColor(getResources().getColor(R.color.colorTxtNormal));
+        mIvTrack.setImageResource(R.drawable.ic_track_normal);
+        mTvTrack.setTextColor(getResources().getColor(R.color.colorTxtNormal));
+        mIvMine.setImageResource(R.drawable.ic_mine_normal);
+        mTvMine.setTextColor(getResources().getColor(R.color.colorTxtNormal));
+
     }
 }
