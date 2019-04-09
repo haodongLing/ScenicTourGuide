eff7b04d98acf124a1449dd38260f34e58e4a84c
linghailong <linghailong@amberweather.com>
2019-04-09 16:23:16 +0800

    leakcaneary

--- app/src/main/java/com/haodong/scenictourguide/GuideApp.java
+++ app/src/main/java/com/haodong/scenictourguide/GuideApp.java
@@ -2,12 +2,15 @@ package com.haodong.scenictourguide;
 
 import android.app.Application;
 import android.app.Service;
+import android.content.Context;
 import android.os.Vibrator;
 
 import com.baidu.mapapi.SDKInitializer;
 import com.haodong.scenictourguide.common.app.TourGuide;
 import com.haodong.scenictourguide.common.net.QueryCityInfoUtils;
 import com.haodong.scenictourguide.service.LocationService;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 /**
  * describe :
@@ -33,5 +36,12 @@ public class GuideApp extends Application {
         locationService = new LocationService(getApplicationContext());
         mVibrator =(Vibrator)getApplicationContext().getSystemService(Service.VIBRATOR_SERVICE);
         SDKInitializer.initialize(getApplicationContext());
+        refWatcher= LeakCanary.install(this);
     }
+    RefWatcher refWatcher;
+    public static RefWatcher getRefWatcher(Context context) {
+        GuideApp application = (GuideApp) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
 }
